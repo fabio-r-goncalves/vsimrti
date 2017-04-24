b8af137d71a849addc103abb6488bab547881b72
Fabio Goncalves <b7207@algoritmi.uminho.pt>
2017-04-24 15:19:28 +0100

    Moved sumo to vsim folder

--- script.sh
+++ script.sh
@@ -6,6 +6,8 @@ echo "empty"
 else 
 mv /home/vsimrti-allinone/vsimrti/* /home/vsimrti
 yes "y" | bash /home/vsimrti/bin/fed/ns3/ns3_installer.sh
+mv /sumo-0.29.0/ /home/vsimrti/sumo
+cd /home/vsimrti/sumo && make && make install
 chown -R 1000:1000 /home/vsimrti
 fi
 top -b
